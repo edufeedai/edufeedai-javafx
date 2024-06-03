eaf2416d66c9faa573b7744753fdacdfc975eafd
Arturo Candela <a.candelamolto@edu.gva.es>
2024-06-04 00:13:20 +0200

    Tengo algún error a la hora de generar el fichero de las entregas, con el zip, pero ahora estoy muy cansado

---
+++ src/main/java/com/github/amiguetes/edufeedai/edufeedaijavafx/GenerateFeedbackFileForStudents.java
@@ -0,0 +1,4 @@
+package com.github.amiguetes.edufeedai.edufeedaijavafx;
+
+public class GenerateFeedbackFileForStrudents {
+}
---
+++ src/test/java/com/github/amiguetes/edufeedai/edufeedaijavafx/GenerateFeedbackFileForStudentsTest.java
@@ -0,0 +1,11 @@
+package com.github.amiguetes.edufeedai.edufeedaijavafx;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class GenerateFeedbackFileForStudentsTest {
+
+    @Test
+    void generateFeedbackFile() {
+    }
+}
