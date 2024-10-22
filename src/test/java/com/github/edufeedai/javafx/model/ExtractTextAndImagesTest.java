4f5500b77deea0591d10f4142d524ac7ae894a7d
Arturo Candela <a.candelamolto@edu.gva.es>
2024-10-22 14:13:44 +0200

    Primera versión del extractor de PDF funcionando, queda muchoo trabajo

---
+++ src/test/java/com/github/edufeedai/javafx/model/ExtractTextAndImagesTest.java
@@ -0,0 +1,11 @@
+package com.github.edufeedai.javafx.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ExtractTextAndImagesTest {
+
+    @Test
+    void extractImagesAndTextFromPDFFile() {
+    }
+}
