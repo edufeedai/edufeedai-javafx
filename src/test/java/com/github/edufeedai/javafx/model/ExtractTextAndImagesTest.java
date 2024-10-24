4f31039b59561956a0adb12946452ef82287ff81
Arturo Candela <a.candelamolto@edu.gva.es>
2024-10-24 06:46:28 +0200

    Después de arreglar los problemas con las claves de azure y dar de alta adecuadamente los recursos, he descubierto que hay un problema a la hora de enviar la imagen, lo investigaré refs #4

--- src/test/java/com/github/edufeedai/javafx/model/ExtractTextAndImagesTest.java
+++ src/test/java/com/github/edufeedai/javafx/model/ExtractTextAndImagesTest.java
@@ -2,8 +2,11 @@ package com.github.edufeedai.javafx.model;
 
 import com.github.edufeedai.javafx.model.exceptions.PDFExtractTextAndImageException;
 import com.github.edufeedai.javafx.model.ocrlib.OCRProcessor;
+import com.github.edufeedai.javafx.model.ocrlib.OCRProcessorAzure;
 import com.github.edufeedai.javafx.model.ocrlib.OCRTesseract;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -14,8 +17,29 @@ class ExtractTextAndImagesTest {
 
     String assessmenetPDFFile = Dotenv.load().get("ASSESSMENT_PDF_FILE");
 
+    String azureCognitiveServicesKey = Dotenv.load().get("AZURE_COGNITIVE_SERVICES_KEY");
+    String azureCognitiveServicesEndpoint = Dotenv.load().get("AZURE_COGNITIVE_SERVICES_ENDPOINT");
+
     @Test
-    void extractImagesAndTextFromPDFFile() {
+    @DisplayName("Extraer imágenes y texto de un archivo PDF con Tesseract")
+    void extractImagesAndTextFromPDFFileTesseract() {
+
+        try {
+
+            OCRProcessor ocrProcessor = new OCRProcessorAzure(azureCognitiveServicesKey, azureCognitiveServicesEndpoint);
+
+            PDFExtractTextAndImagesOrdered extractTextAndImagesOrdered = new PDFExtractTextAndImagesOrdered(ocrProcessor);
+            extractTextAndImagesOrdered.extractImagesAndTextFromPDFFile(assessmenetPDFFile);
+
+        } catch (PDFExtractTextAndImageException e) {
+            fail(e);
+        }
+
+    }
+
+    @Test
+    @DisplayName("Extraer imágenes y texto de un archivo PDF con Azure Cognitive Services")
+    void extractImagesAndTextFromPDFFileAzure() {
 
         try {
 
