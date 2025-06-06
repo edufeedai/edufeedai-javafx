f0e1de13e3f32a54fc81b43f02aa2e09b9824fbf
Arturo Candela <a.candelamolto@edu.gva.es>
2025-06-06 15:05:01 +0000

    Agregado mockito y un entrypoint para la app

---
+++ src/main/java/com/github/edufeedai/App.java
@@ -0,0 +1,9 @@
+package com.github.edufeedai;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Buenos días, bienvenido a la futra app");
+    }
+    
+}
